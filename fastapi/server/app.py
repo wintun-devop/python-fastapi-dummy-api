5147d282b99b8b9e460d9c90d3daa1478b77ca4e
wintun-devop <wintun.hlaing@outlook.com>
2025-10-05 17:05:42 +0630

    api_restructure_complete

--- fastapi/server/app.py
+++ fastapi/server/app.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 from routes.default import router as default_route
 
+#
+from resources.api_paths import TEST_END_POINT
 
 def create_app() -> FastAPI:
     app = FastAPI()
-    app.include_router(default_route, prefix="/user")
+    app.include_router(default_route, prefix=TEST_END_POINT)
     return app
 
 app = create_app()
