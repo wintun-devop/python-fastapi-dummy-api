5bd1332dd0850dea1d6a0591ac1bd2ede6a48b25
wintun-devop <wintun.hlaing@outlook.com>
2025-10-25 17:40:11 +0630

    inventory_model_add

--- fastapi/server/models/__init__.py
+++ fastapi/server/models/__init__.py
@@ -6,7 +6,8 @@ from sqlalchemy import (
                         Text,
                         ForeignKey,
                         func,
-                        UniqueConstraint
+                        UniqueConstraint,
+                        DateTime
                         )
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase
@@ -26,5 +27,16 @@ class User(Base):
     email:Mapped[str] = mapped_column(String,nullable=False,unique=True)
     password:Mapped[str] = mapped_column(String,nullable=False)
     role:Mapped[str] = mapped_column(String,nullable=True,default="user")
-    createdAt: Mapped[datetime.datetime] = mapped_column(default=func.now())
-    updatedAt: Mapped[datetime.datetime] = mapped_column(default=func.now(),onupdate=func.now())
+    createdAt: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), default=datetime.utcnow)
+    updatedAt: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+class Inventory(Base):
+    __tablename__ = "esm_inventory"
+    id:Mapped[str] = mapped_column(Text,primary_key=True)
+    name:Mapped[str] = mapped_column(Text,nullable=False)
+    model_no:Mapped[str] = mapped_column(String,nullable=False, unique=True)
+    price:Mapped[float] = mapped_column(Float,nullable=True,default=0)
+    qty:Mapped[int] = mapped_column(Integer,nullable=True,default=0)
+    createdAt: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), default=datetime.utcnow)
+    updatedAt: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), default=datetime.utcnow, onupdate=datetime.utcnow)
