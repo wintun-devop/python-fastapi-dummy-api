f3217c5bded47b22f21fb59c69ff8c867ef50f83
wintun-devop <wintun.hlaing@outlook.com>
2025-11-01 15:14:39 +0630

    register_success

--- fastapi/server/app.py
+++ fastapi/server/app.py
@@ -3,13 +3,17 @@ from routes.default import router as default_route
 from routes.dummy import dummy_router as dummy_route
 from routes.users import user_router as user_route
 from routes.inventory import inventory_router as inventory_route
+from routes.auth.register import user_register_router as user_register_route
+
+
 
 #End Points
 from resources.api_paths import (
     TEST_END_POINT,
     DUMMY_END_POINT,
     USER_END_POINT,
-    INVENTORY_END_POINT
+    INVENTORY_END_POINT,
+    USER_REGISTER_END_POINT
 )
 
 def create_app() -> FastAPI:
@@ -18,6 +22,7 @@ def create_app() -> FastAPI:
     app.include_router(dummy_route, prefix=DUMMY_END_POINT)
     app.include_router(user_route, prefix=USER_END_POINT)
     app.include_router(inventory_route, prefix=INVENTORY_END_POINT)
+    app.include_router(user_register_route, prefix=USER_REGISTER_END_POINT)
     @app.get("/", include_in_schema=False)
     async def root_redirect():
         return {"docs": "/docs", "openapi": "/openapi.json"}
--- fastapi/server/resources/api_paths.py
+++ fastapi/server/resources/api_paths.py
@@ -9,3 +9,7 @@ TEST_END_POINT = f"{base_path}/test"
 DUMMY_END_POINT = f"{base_path}/dummy"
 USER_END_POINT =f"{base_path}/user"
 INVENTORY_END_POINT =f"{base_path}/inventory"
+
+#auth
+USER_REGISTER_END_POINT = f"{base_path}/auth/register"
+USER_LOGIN_END_POINT = f"{base_path}/auth/login"
